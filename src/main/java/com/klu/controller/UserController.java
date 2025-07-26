419ae44a465d6fa8703a61ba327ce273f898e0d1
el2000030568 <mogilisettilokesh@gmail.com>
2025-07-26 13:32:03 +0530

    u

--- src/main/java/com/klu/controller/UserController.java
+++ src/main/java/com/klu/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.klu.controller;
 
-import org.springframework.beans.factory.annotation.Autowired; 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -8,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.klu.manger.Roomatesmodel;
 
-@RestController
+@Controller
 @RequestMapping("/api")
 public class UserController {
 	
