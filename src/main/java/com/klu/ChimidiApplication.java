86ff773ad2ec51676ed584035986131de6657617
el2000030568 <mogilisettilokesh@gmail.com>
2025-07-26 13:17:32 +0530

    changes

--- src/main/java/com/klu/ChimidiApplication.java
+++ src/main/java/com/klu/ChimidiApplication.java
@@ -2,9 +2,16 @@ package com.klu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ChimidiApplication {
+public class ChimidiApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(ChimidiApplication.class);
+    }
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChimidiApplication.class, args);
