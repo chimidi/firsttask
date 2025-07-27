f2128c67263f740857f42bc96e7efc0130ff8022
el2000030568 <mogilisettilokesh@gmail.com>
2025-07-27 10:07:00 +0530

    update

--- src/main/java/com/klu/controller/UserController.java
+++ src/main/java/com/klu/controller/UserController.java
@@ -1,52 +1,45 @@
 package com.klu.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.klu.manger.Roomatesmodel;
+
+import jakarta.servlet.http.HttpSession;
 
 @RestController
 @RequestMapping("/api")
 @CrossOrigin(origins = "https://firsttask-production.up.railway.app", allowCredentials = "true")
 public class UserController {
-	
-	@Autowired
-	Roomatesmodel rm;
-	
-	static String name;
-	
-	@GetMapping("/login/{pass}/{name}")
-	public String getlogin(@PathVariable("pass") int password,@PathVariable("name") String name)
-	{
-		String check = rm.getname(password, name);
-		if(check!=null) {
-			UserController.name = check;
-			return "home.jsp";
-		}
-		else
-			return "error.jsp";
-		
-	}
-	@GetMapping("/username")
-	public String getname() {
-		return "My name is Tanjusha piyaa";
 
-	}
-	
-	@GetMapping("/loginstatus")
-	public String loginstatus()
-	{
-		if(name!=null){
-			return name;
-		}
-		else
-			return "index.jsp";
-	}
-	
+    @Autowired
+    Roomatesmodel rm;
 
+    @GetMapping("/login/{pass}/{name}")
+    public String getLogin(@PathVariable("pass") int password, @PathVariable("name") String name, HttpSession session) {
+        String check = rm.getname(password, name);
+        if (check != null) {
+            session.setAttribute("username", check); // Store in session
+            session.setMaxInactiveInterval(600); // 10 minutes
+            return "home.jsp";
+        } else {
+            return "error.jsp";
+        }
+    }
+
+    @GetMapping("/username")
+    public String getName(HttpSession session) {
+        String user = (String) session.getAttribute("username");
+        return user != null ? user : "Guest";
+    }
+
+    @GetMapping("/loginstatus")
+    public String loginStatus(HttpSession session) {
+        String user = (String) session.getAttribute("username");
+        if (user != null) {
+            return user;
+        } else {
+            return "index.jsp"; // Not logged in or session expired
+        }
+    }
 }
