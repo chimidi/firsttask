f0ef2d6b1ae1b0f9641b7d345aa6fee2d8d56178
el2000030568 <mogilisettilokesh@gmail.com>
2025-07-27 09:30:33 +0530

    update

--- src/main/java/com/klu/controller/UserController.java
+++ src/main/java/com/klu/controller/UserController.java
@@ -2,6 +2,7 @@ package com.klu.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +12,7 @@ import com.klu.manger.Roomatesmodel;
 
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "https://firsttask-production.up.railway.app", allowCredentials = "true")
 public class UserController {
 	
 	@Autowired
